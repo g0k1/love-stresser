b97e4471ddbc63faadd6f392b5c08eb99ce634d1
StingAving <103068363+St1ngAving@users.noreply.github.com>
2024-09-16 15:55:05 +0200

    Update authorization.php

--- love-stresser.me/authentication/authorization.php
+++ love-stresser.me/authentication/authorization.php
@@ -1,4 +1,5 @@
 <?php
+// Made by Meandoyou and StingAving https://github.com/g0k1/love-stresser
 session_start();
 include("../componements/php/root_domain.php");
 if (isset($_COOKIE['LS_ASP']) && !empty($_COOKIE['LS_ASP'])) {
