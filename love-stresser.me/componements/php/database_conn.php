fe61127db9452c538bd12f21b58473132830e57a
StingAving <103068363+St1ngAving@users.noreply.github.com>
2024-09-16 15:53:37 +0200

    Update database_conn.php

--- love-stresser.me/componements/php/database_conn.php
+++ love-stresser.me/componements/php/database_conn.php
@@ -1,4 +1,5 @@
 <?php
+// Made by Meandoyou and StingAving https://github.com/g0k1/love-stresser
 $servername = "localhost";
 $username = "YOUR_DATABASE_USER";
 $password = "YOUR_DATABASE_PASSWORD";
