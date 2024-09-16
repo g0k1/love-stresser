fa4cc8228e7ce2778884558017eaf1b2e84f6835
StingAving <103068363+St1ngAving@users.noreply.github.com>
2024-09-16 15:53:46 +0200

    Update discord_server.php

--- love-stresser.me/componements/php/discord_server.php
+++ love-stresser.me/componements/php/discord_server.php
@@ -1,4 +1,5 @@
 <?php
+// Made by Meandoyou and StingAving https://github.com/g0k1/love-stresser
  $ban_server_full_link = 'YOUR_DISCORD_INVITATION';
  $server_full_link = 'YOUR_DISCORD_INVITATION';
  $serverInviteCode = 'YOUR_INVITE_CODE';
