512e4b6d11212ed458b284077df2d7cf2754146f
StingAving <103068363+St1ngAving@users.noreply.github.com>
2024-09-16 15:52:00 +0200

    Update index.php

--- love-stresser.me/home/index.php
+++ love-stresser.me/home/index.php
@@ -1,4 +1,5 @@
 <?php
+// Made by Meandoyou and StingAving https://github.com/g0k1/love-stresser
 include("../componements/php/discord_server.php");
 function isMobileDevice() {
     return preg_match('/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i', $_SERVER['HTTP_USER_AGENT']);
